55db5528ae850ba12898cc9470ccf5a3984847aa
sudhanshu.mishra <sudhanshu.mishra@deskera.org>
2024-11-16 23:23:51 +0530

    cors changes in server.js

--- backend/server.js
+++ backend/server.js
@@ -9,13 +9,6 @@ app.use(cors(
     {
     origin: ['https://smart-garage-frontend-orpin.vercel.app'],
     methods: ["POST","GET", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    "headers": [
-        { "key": "Access-Control-Allow-Credentials", "value": "true" },
-        { "key": "Access-Control-Allow-Origin", "value": "*" },
-        { "key": "Access-Control-Allow-Methods", "value": "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-        { "key": "Access-Control-Allow-Headers", "value": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
-        ],
-
     credentials: true
     }
 ));
